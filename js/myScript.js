a490828cbae0e3a68957bf4e8eb49ed51bdccd54
Robert Camp <robert.camp@outlook.com>
2013-09-25 16:59:51 -0600

    first commit added 1st lightbox and open/close functionality

---
+++ js/myScript.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+//opening and closing the lightbox to add quotes
+//
+$('.addContentButton').on('click', function(){
+	$('.lightbox').css('display', 'block');
+});
+
+$('.closeButton').on('click', function(){
+	$('.lightbox').css('display', 'none')
+});
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
